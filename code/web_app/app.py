9af287aa3ee37b0420ac352ec78c0ac44210a18c
kerfallaCisse <95145833+kerfallaCisse@users.noreply.github.com>
2023-04-05 02:23:37 +0200

    chore: add flask app

---
+++ code/web_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route("/")
+def home_page():
+    return render_template('home_page.html')
+
+@app.route("/documentation")
+def documentation():
+    return render_template('documentation.html')
+
+if __name__=="__main__":
+    app.run(host="localhost", port=8080, debug=True)
