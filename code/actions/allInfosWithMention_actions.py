e1811fa3cf75793fa45ce5a49a0ab107bcd08054
kerfallaCisse <95145833+kerfallaCisse@users.noreply.github.com>
2023-05-25 02:23:14 +0200

    feat: adding triple to turtle file

--- code/actions/allInfosWithMention_actions.py
+++ code/actions/allInfosWithMention_actions.py
@@ -5,13 +5,10 @@ from rasa_sdk.events import SlotSet
 from SPARQLWrapper import SPARQLWrapper2
 import sys
 from functions.resolve_mention import mapping
-from functions.graph import getGraph
 from rdflib import Graph
-from rdflib.plugins.sparql import prepareQuery
 import json
 sys.path.insert(0, "/code/functions")
 
-g = getGraph()
 sparql_dbpedia = SPARQLWrapper2(
     "https://dbpedia.org/sparql")  # DBpedia endpoint
 local_endpoint = SPARQLWrapper2(
@@ -27,26 +24,24 @@ class ListerRestaurants(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        limitation = tracker.get_slot("query_limit")
-        
+        limit_query = tracker.get_slot("query_limit")
+        if int(limit_query) > 20:
+            dispatcher.utter_message(response="utter_limit")
+            return []
 
-        query = prepareQuery("""
-                             prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-                             prefix ns0: <http://www.geonames.org/ontology#> 
-                             prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> 
-                             prefix dbpedia: <http://dbpedia.org/resource/> 
-                             SELECT distinct ?rn
-                             WHERE {
-                                ?r ns0:name ?rn
-                             } limit 10
-                             """)
-
-        resp = g.query(query)
-        resp_js = json.loads(resp.serialize(format="json").decode("utf-8"))
+        local_endpoint.setQuery(f"""
+                                prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                                prefix ns0: <http://www.geonames.org/ontology#>
+                                prefix dbpedia: <http://dbpedia.org/resource/>
+                                SELECT ?rn
+                                WHERE{{
+                                    ?r ns0:name ?rn
+                                }} limit {limit_query}
+                                """)
+        results = local_endpoint.query().bindings
         restaurants = list()
-        for result in resp_js['results']['bindings']:
-            for _, v in result.items():
-                restaurants.append(v['value'])
+        for result in results:
+            restaurants.append(result["rn"].value)
 
         response_to_return = ""
 
@@ -55,7 +50,6 @@ class ListerRestaurants(Action):
 
         response_to_return += "..\n\ntu peux spécifier ta recherche, car il y a beaucoup de restaurants"
         dispatcher.utter_message(text=response_to_return)
-        dispatcher.utter_message(text=f"{limitation}")
 
         return [SlotSet("mention_list", restaurants)]
 
@@ -70,6 +64,7 @@ class ResolveMention(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         mention_list = list()
+        restaurant = ""
         try:
             mention_list = tracker.get_slot('mention_list')
             if mention_list == None:
@@ -82,120 +77,121 @@ class ResolveMention(Action):
             return []
         mention = tracker.get_slot('mention')
         index = mapping(mention)
-        restaurant = mention_list[index]
+        try:
+            restaurant = mention_list[index]
 
-        local_endpoint.setQuery(f"""
-                        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                        prefix ns0: <http://www.geonames.org/ontology#>
-                        prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-                        prefix dbpedia: <http://dbpedia.org/resource/>
-                        SELECT ?p ?o
-                        WHERE {{
-                            ?r ns0:name "{restaurant}" .
-                            ?r ?p ?o .
-                            FILTER (?p != rdfs:isDefinedBy && ?p != ns0:featureClass && ?p != ns0:featureCode && ?p != ns0:countryCode && ?p != ns0:parentCountry && ?p != ns0:name && ?p != rdf:type)
-                        }}
-                        """)
-
-        lat = ""
-        long = ""
-        more_infos = ""
-        location_map = ""
-        locationMap_nearby_place = list()
-        dbpedia = ""
-        nearBy_placeList = list()
-        # pour stocker les endroits proche d'un restaurant et la ressource vers la carte pour le lieu correspondant
-        nearBy_placeAndMap = ""
-
-        response = local_endpoint.query().bindings
-        if len(response) == 0:
-            dispatcher.utter_message(
-                text="Désolé nous n'avons aucune information concernant ce restaurant")
-
-        else:
-            for result in response:
-                value_object = result['o'].value
-                if value_object.find("dbpedia") != -1:
-                    dbpedia = value_object
-                    # On récupère quelques informations concernant le restaurant
-                    sparql_dbpedia.setQuery(f"""
-                                            select ?abstract
-                                            where {{
-                                                <{dbpedia}> dbo:abstract ?abstract .
-                                                FILTER(lang(?abstract)="en") 
-                                            }}
-                                            """)
-
-                    resp = sparql_dbpedia.query().bindings
-                    if len(resp) != 0:
-                        for r in resp:
-                            more_infos += r['abstract'].value + " (Anglais)."
-                else:
-                    value_object = value_object.lower()
-
-                value_predicate = (result['p'].value).lower()
-
-                if value_predicate.find("lat") != -1:
-                    lat = value_object + "°N"
-
-                if value_predicate.find("long") != -1:
-                    long = value_object + "°E"
-
-                if value_predicate.find("locationmap") != -1:
-                    location_map = value_object
-
-                if value_predicate.find("nearby") != -1:
-                    nearBy_rdf = value_object
-                    # On parse le graph et on le parcours
-                    g_nearby = Graph().parse(nearBy_rdf)
-
-                    q = """
+            local_endpoint.setQuery(f"""
                             prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                             prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                             prefix ns0: <http://www.geonames.org/ontology#>
-                            SELECT ?s ?p ?o
-                            WHERE {
-                                ?s rdf:type ns0:Feature .
-                                ?s ?p ?o
-                            }
-                    """
-                    resp = g_nearby.query(q)
-                    resp_js = json.loads(resp.serialize(
-                        format="json").decode("utf-8"))
+                            prefix dbpedia: <http://dbpedia.org/resource/>
+                            SELECT ?p ?o
+                            WHERE {{
+                                ?r ns0:name "{restaurant}" .
+                                ?r ?p ?o .
+                                FILTER (?p != rdfs:isDefinedBy && ?p != ns0:featureClass && ?p != ns0:featureCode && ?p != ns0:countryCode && ?p != ns0:parentCountry && ?p != ns0:name && ?p != rdf:type)
+                            }}
+                            """)
 
-                    for result in resp_js['results']['bindings']:
-                        for k, v in result.items():
-                            if k == "s":
-                                placeMap = v['value']
-                                if placeMap not in locationMap_nearby_place:
-                                    locationMap_nearby_place.append(placeMap)
+            lat = ""
+            long = ""
+            more_infos = ""
+            location_map = ""
+            locationMap_nearby_place = list()
+            dbpedia = ""
+            nearBy_placeList = list()
+            # pour stocker les endroits proche d'un restaurant et la ressource vers la carte pour le lieu correspondant
+            nearBy_placeAndMap = ""
 
-                            if k == "o" and v['type'] == "literal":
-                                # On récupère le nom des endroits qui sont proche du restaurant. Attention, ces endroits ne sont pas des restaurant
-                                # Ils font partie d'une autre classe
-                                nearby_place_name = v['value']
-                                if nearby_place_name not in nearBy_placeList:
-                                    nearBy_placeList.append(nearby_place_name)
-
-            # Les deux listes auront toujours la même taille, car les données sont récupérés du résultats de la requête précédente
-            for i in range(len(nearBy_placeList)):
-                nearBy_placeAndMap += "- " + \
-                    nearBy_placeList[i] + "--> " + \
-                    locationMap_nearby_place[i] + "\n"
-
-            # On vérifie qu'on possède les données
-            if len(more_infos) != 0:
-                dispatcher.utter_message(text=more_infos)
-            if len(lat) != 0 and len(long) != 0:
-                text = f"""Sa latitude est de {lat} et sa longitude est de {long}."""
-                dispatcher.utter_message(text=text)
-            if len(location_map) != 0:
+            response = local_endpoint.query().bindings
+            if len(response) == 0:
                 dispatcher.utter_message(
-                    text=f"Visualisation du restaurant {restaurant} sur une carte: {location_map}")
-            if len(nearBy_placeAndMap) != 0:
-                dispatcher.utter_message(
-                    text=f"Le restaurant {restaurant} est proche des lieux suivants:\n\n{nearBy_placeAndMap}")
+                    text="Désolé nous n'avons aucune information concernant ce restaurant")
+
+            else:
+                for result in response:
+                    value_object = result['o'].value
+                    if value_object.find("dbpedia") != -1:
+                        dbpedia = value_object
+                        # On récupère quelques informations concernant le restaurant
+                        sparql_dbpedia.setQuery(f"""
+                                                select ?abstract
+                                                where {{
+                                                    <{dbpedia}> dbo:abstract ?abstract .
+                                                    FILTER(lang(?abstract)="en") 
+                                                }}
+                                                """)
+
+                        resp = sparql_dbpedia.query().bindings
+                        if len(resp) != 0:
+                            for r in resp:
+                                more_infos += r['abstract'].value + " (Anglais)."
+                    else:
+                        value_object = value_object.lower()
+
+                    value_predicate = (result['p'].value).lower()
+
+                    if value_predicate.find("lat") != -1:
+                        lat = value_object + "°N"
+
+                    if value_predicate.find("long") != -1:
+                        long = value_object + "°E"
+
+                    if value_predicate.find("locationmap") != -1:
+                        location_map = value_object
+
+                    if value_predicate.find("nearby") != -1:
+                        nearBy_rdf = value_object
+                        # On parse le graph et on le parcours
+                        g_nearby = Graph().parse(nearBy_rdf)
+
+                        q = """
+                                prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                                prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                                prefix ns0: <http://www.geonames.org/ontology#>
+                                SELECT ?s ?p ?o
+                                WHERE {
+                                    ?s rdf:type ns0:Feature .
+                                    ?s ?p ?o
+                                }
+                        """
+                        resp = g_nearby.query(q)
+                        resp_js = json.loads(resp.serialize(
+                            format="json").decode("utf-8"))
+
+                        for result in resp_js['results']['bindings']:
+                            for k, v in result.items():
+                                if k == "s":
+                                    placeMap = v['value']
+                                    if placeMap not in locationMap_nearby_place:
+                                        locationMap_nearby_place.append(placeMap)
+
+                                if k == "o" and v['type'] == "literal":
+                                    # On récupère le nom des endroits qui sont proche du restaurant. Attention, ces endroits ne sont pas des restaurant
+                                    # Ils font partie d'une autre classe
+                                    nearby_place_name = v['value']
+                                    if nearby_place_name not in nearBy_placeList:
+                                        nearBy_placeList.append(nearby_place_name)
+
+                # Les deux listes auront toujours la même taille, car les données sont récupérés du résultats de la requête précédente
+                for i in range(len(nearBy_placeList)):
+                    nearBy_placeAndMap += "- " + \
+                        nearBy_placeList[i] + "--> " + \
+                        locationMap_nearby_place[i] + "\n"
+
+                # On vérifie qu'on possède les données
+                if len(more_infos) != 0:
+                    dispatcher.utter_message(text=more_infos)
+                if len(lat) != 0 and len(long) != 0:
+                    text = f"""Sa latitude est de {lat} et sa longitude est de {long}."""
+                    dispatcher.utter_message(text=text)
+                if len(location_map) != 0:
+                    dispatcher.utter_message(
+                        text=f"Visualisation du restaurant {restaurant} sur une carte: {location_map}")
+                if len(nearBy_placeAndMap) != 0:
+                    dispatcher.utter_message(
+                        text=f"Le restaurant {restaurant} est proche des lieux suivants:\n\n{nearBy_placeAndMap}")
+        except IndexError:
+            return []
 
         return []
-
--- code/actions/restaurantsInCity_actions.py
+++ code/actions/restaurantsInCity_actions.py
@@ -4,9 +4,15 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from SPARQLWrapper import SPARQLWrapper2
 import requests
+from functions.graph import getGraph
+from rdflib import Literal, Namespace, URIRef, Graph
+from rdflib.namespace import RDF, GEO
 
 osmap_endpoint = SPARQLWrapper2(
     "https://sophox.org/sparql")  # Open street map endpoint
+osmap_endpoint.setTimeout(60)  # query time out for the endpoint in seconde
+GRAPH = Graph()
+
 
 class RestaurantsInCity(Action):
 
@@ -18,69 +24,112 @@ class RestaurantsInCity(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         city = tracker.get_slot("city")
-        limit = tracker.get_slot("query_limit")
-        cities = process_file("data/city.txt") # pour la liste des villes
-        rest_names = process_file("data/rest_name.txt") # pour la liste des restaurants
-        
+        print(city)
+        limit_query = tracker.get_slot("query_limit")
+        limit = int(limit_query)
+
+        if limit > 20:
+            dispatcher.utter_message(
+                response="utter_limit")
+            return []
+
+        cities = process_file("data/city.txt")  # pour la liste des villes
+        # pour la liste des restaurants
+        rest_names = process_file("data/rest_name.txt")
+
         if city not in cities:
-            dispatcher.utter_message(text="Désolé nous n'avons trouvé aucun restaurant dans cette ville\n\nIl se peut que la ville mentionnée ne soit pas une ville suisse")
+            dispatcher.utter_message(
+                text="Désolé nous n'avons trouvé aucun restaurant dans cette ville\n\nIl se peut que la ville mentionnée ne soit pas une ville suisse")
             return []
-        
-        dispatcher.utter_message(text=f"limit pour restaurants dans une ville: {limit}")
+
         restaurants = list()
-        osmap_endpoint.setQuery(f"""
-                                SELECT ?osmn ?type ?loc ?name ?street ?cuisine ?hours
-                                WHERE {{
-                                    VALUES ?type {{"bar" "restaurant" "pub" "cafe" "food_court" "biergarten" "fast_food"}}
-                                    ?osmn osmt:addr:city "{city}";
-                                        osmt:amenity ?type ;
-                                        osmm:loc ?loc ;
-                                        osmt:name ?name ;
-                                        osmt:addr:street ?street .
-                                    OPTIONAL {{
-                                        ?osmn osmt:cuisine ?cuisine ;
-                                              osmt:opening_hours ?hours . 
-                                    }}    
-                                }}
-                                """)
-        results = osmap_endpoint.query().bindings
-        if len(results) == 0:
-            dispatcher.utter_message(text="Désolé nous n'avons trouvé aucun restaurant dans cette ville")
+        restaurants_mention_list = list()
+
+        try:
+            osmap_endpoint.setQuery(f"""
+                                    SELECT ?osmn ?type ?loc ?name ?street ?cuisine ?hours
+                                    WHERE {{
+                                        VALUES ?type {{"bar" "restaurant" "pub" "cafe" "food_court" "biergarten" "fast_food"}}
+                                        ?osmn osmt:addr:city "{city}";
+                                            osmt:amenity ?type ;
+                                            osmm:loc ?loc ;
+                                            osmt:name ?name ;
+                                            osmt:addr:street ?street .
+                                        OPTIONAL {{
+                                            ?osmn osmt:cuisine ?cuisine ;
+                                                osmt:opening_hours ?hours . 
+                                        }}    
+                                    }} limit {limit_query}
+                                    """)
+            results = osmap_endpoint.query().bindings
+            if len(results) == 0:
+                dispatcher.utter_message(
+                    text="Désolé nous n'avons trouvé aucun restaurant dans cette ville")
+                return []
+            l = 0
+            for result in results:
+                if l == limit:
+                    break
+
+                rest_name = result['name'].value
+                osmn = result["osmn"].value
+                rest_street = result["street"].value
+                rest_loc = result["loc"].value
+                rest_slots = f"{rest_name} {rest_loc}"
+                restaurants.append(rest_slots)
+                restaurants_mention_list.append(rest_name)
+                resp = str(l+1)
+                dispatcher.utter_message(
+                    text=f"{resp}- Nom du restaurant: {rest_name}\n- Adresse: {rest_street}")
+                # On récupère les valeurs optionnelles
+                try:
+                    cuisine = result["cuisine"].value
+                    opening_hours = result["hours"].value
+                    dispatcher.utter_message(
+                        text=f"- Type de cuisine: {cuisine}\n- Horaires d'ouverture: {opening_hours}")
+                except KeyError:
+                    pass
+
+                if rest_name not in rest_names:
+                    with open("data/rest_name.txt", "a", encoding="utf-8") as f:
+                        # on insère le restaurant dans le fichier pour faciliter l'extraction des entités
+                        f.write(rest_name+"\n")
+                        # On insère le restaurant dans le graphe de connaissance (graphe par défaut)
+                        triples = f"<{osmn}> a <https://www.openstreetmap.org/node/>; ns0:name '{rest_name}'; ns0:parentCountry <https://sws.geonames.org/2658434/> ."
+                        requests.post(url="http://localhost:7200/repositories/POC-1/rdf-graphs/service?default",
+                                      data=triples,
+                                      headers={"Content-Type": "text/turtle"})
+
+                        OSMPNODE = URIRef(
+                            "https://www.openstreetmap.org/node/")
+                        OSMPOBJECT = URIRef(osmn)
+                        PARENTCOUNTRY = URIRef(
+                            "https://sws.geonames.org/2658434/")
+                        NS0 = Namespace("http://www.geonames.org/ontology#")
+                        name = NS0.name
+                        parent_country = NS0.parentCountry
+                        GRAPH.bind("ns0", NS0)
+                        GRAPH.bind("geo", GEO)
+                        GRAPH.add((OSMPOBJECT, RDF.type, OSMPNODE))
+                        GRAPH.add((OSMPOBJECT, name, Literal(rest_name)))
+                        GRAPH.add((OSMPOBJECT, parent_country, PARENTCOUNTRY))
+                        GRAPH.parse(source="restInswitzerland.ttl")
+                        GRAPH.serialize(
+                            destination="restInswitzerland.ttl", format="ttl")
+
+                dispatcher.utter_message(text=">>>>>>>>")
+                l += 1
+        except TimeoutError:
+            dispatcher.utter_message(
+                text="Désolé, la requête prend plus de temps que prévu. Reposez votre question.")
             return []
-        limit = 0
-        for result in results:
-            if limit == 10:
-                break
-            
-            rest_name = result['name'].value
-            osmn = result["osmn"].value
-            rest_street = result["street"].value
-            rest_loc = result["loc"].value
-            rest_slots = f"{rest_name} {rest_loc}"
-            restaurants.append(rest_slots)
-            dispatcher.utter_message(text=f"- Nom du restaurant: {rest_name}\n- Adresse: {rest_street}")  
-            # On récupère les valeurs optionnelles
-            try:
-                cuisine = result["cuisine"].value
-                opening_hours = result["hours"].value
-                dispatcher.utter_message(text=f"- Type de cuisine: {cuisine}\n- Horaires d'ouverture: {opening_hours}")
-            except KeyError:
-                pass   
-            
-            if rest_name not in rest_names:
-                with open("data/rest_name.txt","a",encoding="utf-8") as f: 
-                    f.write(rest_name+"\n") # on insère le restaurant dans le fichier pour faciliter l'extraction des entités
-                    # On insère le restaurant dans le graphe de connaissance (graphe par défaut)
-                    requests.post(url="http://localhost:7200/repositories/POC-1/rdf-graphs/service?default",
-                                    data=f"<{osmn}> a <https://www.openstreetmap.org/node/>; ns0:name '{rest_name}'; ns0:parentCountry <https://sws.geonames.org/2658434/> .",
-                                    headers={"Content-Type":"text/turtle"})
-            dispatcher.utter_message(text=">>>>>>>>")        
-            limit += 1
-        
-        dispatcher.utter_message(text="Peux-tu préciser ta recherche ? Voir la documentation pour les types de questions")
-        return [SlotSet("curiosity", restaurants)]
+
+        dispatcher.utter_message(
+            text="Peux-tu préciser ta recherche ? Voir la documentation pour les types de questions")
+        return [SlotSet("curiosity", restaurants), SlotSet("mention_list", restaurants_mention_list)]
+
 
 def process_file(file_path: str) -> list():
-    with open(file_path,"r",encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         lines = f.readlines()
         return [l.strip() for l in lines]
