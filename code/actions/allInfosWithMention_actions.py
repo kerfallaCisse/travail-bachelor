7a240524f2bd2df86cad52f716dda3ff673c84ac
kerfallaCisse <95145833+kerfallaCisse@users.noreply.github.com>
2023-05-26 02:27:27 +0200

    feat: imrpoving graph knowledge + rest name

--- code/actions/allInfosWithMention_actions.py
+++ code/actions/allInfosWithMention_actions.py
@@ -96,6 +96,10 @@ class ResolveMention(Action):
             lat = ""
             long = ""
             more_infos = ""
+            cuisine = ""
+            opening_hours = ""
+            adresse = ""
+            postcode = ""
             location_map = ""
             locationMap_nearby_place = list()
             dbpedia = ""
@@ -111,7 +115,7 @@ class ResolveMention(Action):
             else:
                 for result in response:
                     value_object = result['o'].value
-                    if value_object.find("dbpedia") != -1:
+                    if value_object.find("dbpedia") > -1:
                         dbpedia = value_object
                         # On récupère quelques informations concernant le restaurant
                         sparql_dbpedia.setQuery(f"""
@@ -125,22 +129,35 @@ class ResolveMention(Action):
                         resp = sparql_dbpedia.query().bindings
                         if len(resp) != 0:
                             for r in resp:
-                                more_infos += r['abstract'].value + " (Anglais)."
+                                more_infos += r['abstract'].value + \
+                                    " (Anglais)."
                     else:
                         value_object = value_object.lower()
 
                     value_predicate = (result['p'].value).lower()
 
-                    if value_predicate.find("lat") != -1:
+                    if value_predicate.find("lat") > -1:
                         lat = value_object + "°N"
 
-                    if value_predicate.find("long") != -1:
+                    if value_predicate.find("long") > -1:
                         long = value_object + "°E"
 
-                    if value_predicate.find("locationmap") != -1:
+                    if value_predicate.find("cuisine") > -1:
+                        cuisine += value_object + "; "
+
+                    if value_predicate.find("opening_hours") > -1:
+                        opening_hours = value_object
+
+                    if value_predicate.find("adresse") > -1:
+                        adresse = value_object
+
+                    if value_predicate.find("postcode") > -1:
+                        postcode = value_object
+
+                    if value_predicate.find("locationmap") > -1:
                         location_map = value_object
 
-                    if value_predicate.find("nearby") != -1:
+                    if value_predicate.find("nearby") > -1:
                         nearBy_rdf = value_object
                         # On parse le graph et on le parcours
                         g_nearby = Graph().parse(nearBy_rdf)
@@ -164,14 +181,16 @@ class ResolveMention(Action):
                                 if k == "s":
                                     placeMap = v['value']
                                     if placeMap not in locationMap_nearby_place:
-                                        locationMap_nearby_place.append(placeMap)
+                                        locationMap_nearby_place.append(
+                                            placeMap)
 
                                 if k == "o" and v['type'] == "literal":
                                     # On récupère le nom des endroits qui sont proche du restaurant. Attention, ces endroits ne sont pas des restaurant
                                     # Ils font partie d'une autre classe
                                     nearby_place_name = v['value']
                                     if nearby_place_name not in nearBy_placeList:
-                                        nearBy_placeList.append(nearby_place_name)
+                                        nearBy_placeList.append(
+                                            nearby_place_name)
 
                 # Les deux listes auront toujours la même taille, car les données sont récupérés du résultats de la requête précédente
                 for i in range(len(nearBy_placeList)):
@@ -185,6 +204,17 @@ class ResolveMention(Action):
                 if len(lat) != 0 and len(long) != 0:
                     text = f"""Sa latitude est de {lat} et sa longitude est de {long}."""
                     dispatcher.utter_message(text=text)
+                if len(cuisine) != 0:
+                    text = f"Type de cuisine du restaruant: {cuisine}"
+                    dispatcher.utter_message(text=text)
+                if len(opening_hours) != 0:
+                    text = f"Horaires d'ouverture: {opening_hours}"
+                    dispatcher.utter_message(text=text)
+                if len(adresse) != 0:
+                    text = f"Rue: {adresse}"
+                    dispatcher.utter_message(text=text)
+                if len(postcode) != 0:
+                    dispatcher.utter_message(text=f"Code postal: {postcode}")
                 if len(location_map) != 0:
                     dispatcher.utter_message(
                         text=f"Visualisation du restaurant {restaurant} sur une carte: {location_map}")
--- code/actions/restaurantsInCity_actions.py
+++ code/actions/restaurantsInCity_actions.py
@@ -24,7 +24,6 @@ class RestaurantsInCity(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         city = tracker.get_slot("city")
-        print(city)
         limit_query = tracker.get_slot("query_limit")
         limit = int(limit_query)
 
@@ -47,7 +46,7 @@ class RestaurantsInCity(Action):
 
         try:
             osmap_endpoint.setQuery(f"""
-                                    SELECT ?osmn ?type ?loc ?name ?street ?cuisine ?hours
+                                    SELECT ?osmn ?type ?loc ?longitude ?latitude ?name ?street ?postcode ?cuisine ?hours
                                     WHERE {{
                                         VALUES ?type {{"bar" "restaurant" "pub" "cafe" "food_court" "biergarten" "fast_food"}}
                                         ?osmn osmt:addr:city "{city}";
@@ -55,9 +54,16 @@ class RestaurantsInCity(Action):
                                             osmm:loc ?loc ;
                                             osmt:name ?name ;
                                             osmt:addr:street ?street .
+                                            BIND(geof:longitude(?loc) as ?longitude)
+                                            BIND(geof:latitude(?loc) as ?latitude)
                                         OPTIONAL {{
-                                            ?osmn osmt:cuisine ?cuisine ;
-                                                osmt:opening_hours ?hours . 
+                                            ?osmn osmt:cuisine ?cuisine .
+                                        }}
+                                        OPTIONAL {{
+                                            ?osmn osmt:opening_hours ?hours . 
+                                        }}
+                                        OPTIONAL{{
+                                            ?osmn osmt:addr:postcode ?postcode .
                                         }}    
                                     }} limit {limit_query}
                                     """)
@@ -75,18 +81,41 @@ class RestaurantsInCity(Action):
                 osmn = result["osmn"].value
                 rest_street = result["street"].value
                 rest_loc = result["loc"].value
+                rest_lat = result["latitude"].value
+                rest_long = result["longitude"].value
                 rest_slots = f"{rest_name} {rest_loc}"
                 restaurants.append(rest_slots)
                 restaurants_mention_list.append(rest_name)
+                all_cuisines = list()
+                cuisine = ""
+                opening_hours = ""
+                postcode = ""
+                triples = f"""<{osmn}> a <https://www.openstreetmap.org/node/> ; geo1:lat "{rest_lat}" ; geo1:long "{rest_long}" ; r:adresse "{rest_street}" ;"""
                 resp = str(l+1)
                 dispatcher.utter_message(
-                    text=f"{resp}- Nom du restaurant: {rest_name}\n- Adresse: {rest_street}")
+                    text=f"{resp}- Nom du restaurant: {rest_name}\n- Rue: {rest_street}")
                 # On récupère les valeurs optionnelles
                 try:
                     cuisine = result["cuisine"].value
                     opening_hours = result["hours"].value
-                    dispatcher.utter_message(
-                        text=f"- Type de cuisine: {cuisine}\n- Horaires d'ouverture: {opening_hours}")
+                    postcode = result["postcode"].value
+                    if len(cuisine) != 0:
+                        if cuisine.find(";") > -1:
+                            all_cuisines = cuisine.split(";")
+                            for c in all_cuisines:
+                                triples += f"""r:cuisine "{c}" ;"""
+                        else:
+                            triples += f"""r:cuisine "{cuisine}" ;"""
+                        dispatcher.utter_message(
+                            text=f"- Type de cuisine: {cuisine}")
+                    if len(opening_hours) != 0:
+                        triples += f"""r:opening_hours "{opening_hours}" ;"""
+                        dispatcher.utter_message(
+                            text=f"- Horaires d'ouvertures: {opening_hours}")
+                    if len(postcode) != 0:
+                        triples += f"""r:postcode "{postcode}" ;"""
+                        dispatcher.utter_message(
+                            text=f"- Code postal: {postcode}")
                 except KeyError:
                     pass
 
@@ -95,10 +124,13 @@ class RestaurantsInCity(Action):
                         # on insère le restaurant dans le fichier pour faciliter l'extraction des entités
                         f.write(rest_name+"\n")
                         # On insère le restaurant dans le graphe de connaissance (graphe par défaut)
-                        triples = f"<{osmn}> a <https://www.openstreetmap.org/node/>; ns0:name '{rest_name}'; ns0:parentCountry <https://sws.geonames.org/2658434/> ."
-                        requests.post(url="http://localhost:7200/repositories/POC-1/rdf-graphs/service?default",
-                                      data=triples,
-                                      headers={"Content-Type": "text/turtle"})
+                        triples += f"""ns0:name "{rest_name}"; ns0:parentCountry <https://sws.geonames.org/2658434/> ."""
+                        ret = requests.post(url="http://localhost:7200/repositories/POC-1/rdf-graphs/service?default",
+                                            data=triples,
+                                            headers={"Content-Type": "text/turtle"})
+                        # print(rest_name,ret.status_code)
+                        # print(ret.content)
+                        # print(ret.raw)
 
                         OSMPNODE = URIRef(
                             "https://www.openstreetmap.org/node/")
@@ -106,13 +138,44 @@ class RestaurantsInCity(Action):
                         PARENTCOUNTRY = URIRef(
                             "https://sws.geonames.org/2658434/")
                         NS0 = Namespace("http://www.geonames.org/ontology#")
-                        name = NS0.name
-                        parent_country = NS0.parentCountry
+                        GEO1 = Namespace(
+                            "http://www.w3.org/2003/01/geo/wgs84_pos#")
+                        R = Namespace("http://restaurant#")
                         GRAPH.bind("ns0", NS0)
                         GRAPH.bind("geo", GEO)
+                        GRAPH.bind("r", R)
+                        cuisine_type = R.cuisine
+                        openingHours = R.opening_hours
+                        post_code = R.postcode
+                        adresse = R.adresse
+                        latitude = GEO1.lat
+                        longitude = GEO1.long
+                        name = NS0.name
+                        parent_country = NS0.parentCountry
+
                         GRAPH.add((OSMPOBJECT, RDF.type, OSMPNODE))
                         GRAPH.add((OSMPOBJECT, name, Literal(rest_name)))
                         GRAPH.add((OSMPOBJECT, parent_country, PARENTCOUNTRY))
+
+                        # On ajoute les triplets dans le fichier turtle avec la même logique
+                        if cuisine.find(";") > -1:
+                            all_cuisines = cuisine.split(";")
+                            for c in all_cuisines:
+                                GRAPH.add(
+                                    (OSMPOBJECT, cuisine_type, Literal(c)))
+                        else:
+                            if len(cuisine) != 0:
+                                GRAPH.add(
+                                    (OSMPOBJECT, cuisine_type, Literal(cuisine)))
+                        if len(opening_hours) != 0:
+                            GRAPH.add((OSMPOBJECT, openingHours,
+                                      Literal(opening_hours)))
+                        if len(postcode) != 0:
+                            GRAPH.add(
+                                (OSMPOBJECT, post_code, Literal(postcode)))
+                        GRAPH.add((OSMPOBJECT, latitude, Literal(rest_lat)))
+                        GRAPH.add((OSMPOBJECT, longitude, Literal(rest_long)))
+                        GRAPH.add((OSMPOBJECT, adresse, Literal(rest_street)))
                         GRAPH.parse(source="restInswitzerland.ttl")
                         GRAPH.serialize(
                             destination="restInswitzerland.ttl", format="ttl")
